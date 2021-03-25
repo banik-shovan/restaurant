c21367a722083de8ca5bb73bf9feb4cffade8e7b
s b <banishovan41@gmail.com>
2021-03-25 09:53:12 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import Maincomponent from './component/MainComponent';
+function App() {
+  return (
+    <div className="App">
+      <Maincomponent />
+    </div>
+  );
+}
+
+export default App;
